9d1e11f25f464837f3087e650da9f8527a0f095f
Shayan <nwryshayan500@gmail.com>
2024-12-12 10:46:43 +0330

    Fuction Signatures (Defining Function parameter and Fuction Type Separately)

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,10 +1,23 @@
 "use strict";
-const logDetails = (uid, item) => {
-    console.log(`${item} has a uid of ${uid}`);
+// let greet: Function;
+//Example 1
+let greet;
+//  no need to define the fuction type V (:void) cause TypeScript automatically detects that we have no return
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
 };
-const greet = (user) => {
-    console.log(`${user.name} says hello!`);
+//Example 2
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
 };
-const greetAgain = (user) => {
-    console.log(`${user.name} says hello!`);
+//Example 3
+let logDetails;
+logDetails = (samurai) => {
+    console.log(`${samurai.name} is ${samurai.age} years old`);
 };
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,14 +1,29 @@
-type StringOrNum = string | number
-type objWithName = {name: string, uid: StringOrNum}
+// let greet: Function;
 
-const logDetails = (uid: StringOrNum, item: string) => {
-    console.log(`${item} has a uid of ${uid}`)
+
+//Example 1
+let greet: (a: string, b: string) => void
+
+//  no need to define the fuction type V (:void) cause TypeScript automatically detects that we have no return
+greet = (name: string, greeting: string) => {
+    console.log(`${name} says ${greeting}`)
 }
 
-const greet = (user: objWithName) => {
-    console.log(`${user.name} says hello!`)
-}
+//Example 2
+let calc: (a: number, b: number, c:string) => number;
 
-const greetAgain = (user: objWithName) => {
-    console.log(`${user.name} says hello!`)
+calc = (numOne: number, numTwo: number, action: string) => {
+    if(action === 'add'){
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo;
+    }
+} 
+
+//Example 3
+let logDetails: (obj: {name: string, age: number}) => void
+
+type person = {name: string, age: number}
+logDetails = (samurai: person) => {
+    console.log(`${samurai.name} is ${samurai.age} years old`)
 }
