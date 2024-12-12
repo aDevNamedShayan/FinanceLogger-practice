156045f5d11225a2d75b8bfe0e07fd2bcfbf5909
Shayan <nwryshayan500@gmail.com>
2024-12-12 09:51:41 +0330

    Function Basics (defining input types and function type)

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log('testiiiiiiiiiiiiiiii');
+// let greet = () => {
+//     console.log("HelloWorld")
+// }
+let greet;
+// greet = 'hello'
+greet = () => {
+    console.log("HelloWorldAgain");
+};
+// optional parameter can be defined like (c?: number | string) but it should not include the default value (= 10)
+//                          Type of fuctions return parameter VV     here there isnt anything being returned so its just void
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, "20");
+//   but here the return type is number VV    because there is a return and its type is number 
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(100, 60);
+// result = "something else"
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,26 @@
-console.log('testiiiiiiiiiiiiiiii')
+// let greet = () => {
+//     console.log("HelloWorld")
+// }
+
+let greet: Function
+
+// greet = 'hello'
+
+greet = () => {
+    console.log("HelloWorldAgain")
+}
+// optional parameter can be defined like (c?: number | string) but it should not include the default value (= 10)
+//                          Type of fuctions return parameter VV     here there isnt anything being returned so its just void
+const add = (a: number, b: number, c: number | string = 10): void => {
+    console.log(a + b)
+    console.log(c)
+}
+
+add(5, 10 , "20")
+//   but here the return type is number VV    because there is a return and its type is number 
+const minus = (a: number, b: number): number => {
+    return a - b
+}
+
+let result = minus(100, 60)
+// result = "something else"
