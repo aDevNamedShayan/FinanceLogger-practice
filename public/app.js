ec97cad8dd6ad1f2c99c2a3ce51c24fb7ad8a786
Shayan <nwryshayan500@gmail.com>
2024-12-12 13:52:50 +0330

    Access Modifiers (Public, Private, Readonly)

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 "use strict";
 //  classes
 class invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} owes $${this.amount} for ${this.details}`;
@@ -15,10 +18,9 @@ const invTwo = new invoice("Mestefa", "KayiPela", 5000);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invOne.client = "Mamad";
-invTwo.amount = 6000;
-console.log(invOne, invTwo);
-console.log(invoices);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 const form = document.querySelector('.new-item-form');
 // inputs
 const type = document.querySelector('#type');
