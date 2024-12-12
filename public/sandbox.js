80a76cddf19cb363e683d40b92eb6d5fca48362d
Shayan <nwryshayan500@gmail.com>
2024-12-12 10:14:51 +0330

    Type Aliases (Componentizing Types)

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,22 +1,10 @@
 "use strict";
-// let greet = () => {
-//     console.log("HelloWorld")
-// }
-let greet;
-// greet = 'hello'
-greet = () => {
-    console.log("HelloWorldAgain");
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
 };
-// optional parameter can be defined like (c?: number | string) but it should not include the default value (= 10)
-//                          Type of fuctions return parameter VV     here there isnt anything being returned so its just void
-const add = (a, b, c = 10) => {
-    console.log(a + b);
-    console.log(c);
+const greet = (user) => {
+    console.log(`${user.name} says hello!`);
 };
-add(5, 10, "20");
-//   but here the return type is number VV    because there is a return and its type is number 
-const minus = (a, b) => {
-    return a - b;
+const greetAgain = (user) => {
+    console.log(`${user.name} says hello!`);
 };
-let result = minus(100, 60);
-// result = "something else"
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,26 +1,14 @@
-// let greet = () => {
-//     console.log("HelloWorld")
-// }
+type StringOrNum = string | number
+type objWithName = {name: string, uid: StringOrNum}
 
-let greet: Function
-
-// greet = 'hello'
-
-greet = () => {
-    console.log("HelloWorldAgain")
-}
-// optional parameter can be defined like (c?: number | string) but it should not include the default value (= 10)
-//                          Type of fuctions return parameter VV     here there isnt anything being returned so its just void
-const add = (a: number, b: number, c: number | string = 10): void => {
-    console.log(a + b)
-    console.log(c)
+const logDetails = (uid: StringOrNum, item: string) => {
+    console.log(`${item} has a uid of ${uid}`)
 }
 
-add(5, 10 , "20")
-//   but here the return type is number VV    because there is a return and its type is number 
-const minus = (a: number, b: number): number => {
-    return a - b
+const greet = (user: objWithName) => {
+    console.log(`${user.name} says hello!`)
 }
 
-let result = minus(100, 60)
-// result = "something else"
+const greetAgain = (user: objWithName) => {
+    console.log(`${user.name} says hello!`)
+}
